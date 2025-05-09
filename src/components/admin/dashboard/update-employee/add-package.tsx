7453911d9d21990d2353457338e7bcf2bf0b827b
Syed Farhan <syedfarhan596@gmail.com>
2025-05-10 00:56:15 +0530

    minor package fix and configs

--- src/components/admin/dashboard/update-employee/add-package.tsx
+++ src/components/admin/dashboard/update-employee/add-package.tsx
@@ -1,43 +1,91 @@
-import { Controller } from "react-hook-form";
-import { MultiSelect, Button, Group } from "@mantine/core";
-import { BgDiv } from "../../../common/style-components/bg-div";
-import { OrganizationConfig } from "../../../../interfaces/organization";
-import { useCustomToast } from "../../../../utils/common/toast";
-import { useRecoilState } from "recoil";
-import { employeePackagesAtom } from "../../../../atoms/employee-atom";
-import { useEffect } from "react";
-import { toast } from "react-toastify";
-import { getAllPackagesByAdmin } from "../../../../services/admin-services";
+import { Controller, useForm } from 'react-hook-form';
+import { MultiSelect, Button, Group } from '@mantine/core';
+import { BgDiv } from '../../../common/style-components/bg-div';
+import { OrganizationConfig } from '../../../../interfaces/organization';
+import { useCustomToast } from '../../../../utils/common/toast';
+import { useRecoilState } from 'recoil';
+import { employeePackagesAtom } from '../../../../atoms/employee-atom';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import {
+  getAllPackagesByAdmin,
+  updateEmployeePackageByAdmin,
+} from '../../../../services/admin-services';
+import {
+  EmployeePackageForm,
+  employeePackageSchema,
+} from '../../../../forms/update-employee';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const PackagesFormComponent = ({
   organizationConfig,
-  control,
-  errors,
+  employeeId,
 }: {
   organizationConfig: OrganizationConfig;
-  control: any;
-  errors: any;
+  employeeId: string;
 }) => {
   const [employmentPackagesOptions, setEmploymentPackagesOptions] =
     useRecoilState(employeePackagesAtom);
 
   const { showSuccessToast } = useCustomToast();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EmployeePackageForm>({
+    resolver: zodResolver(employeePackageSchema),
+  });
 
   useEffect(() => {
     if (employmentPackagesOptions.length === 0) {
       getAllPackagesByAdmin()
-        .then((response) => {
-          const pkgs = response.map((res: any) => ({
-            value: res._id,
-            label: res.title,
-          }));
-          setEmploymentPackagesOptions(pkgs);
+        .then(response => {
+          setEmploymentPackagesOptions(response);
         })
         .catch((error: any) => {
-          toast.error(error?.response?.data?.message || "Something went wrong");
+          toast.error(error?.response?.data?.message || 'Something went wrong');
         });
     }
   }, [employmentPackagesOptions, setEmploymentPackagesOptions]);
+
+  const onSubmit = (data: EmployeePackageForm) => {
+    const formattedData: {
+      employeeId: string;
+      packages: {
+        packageId: string;
+        tasks: { taskId: string; startDate: string }[];
+      }[];
+    } = {
+      employeeId: employeeId,
+      packages: [],
+    };
+
+    data.packagesInfo.forEach(selectedPackageId => {
+      const packageFromAllPackages = employmentPackagesOptions.find(
+        pack => pack._id === selectedPackageId
+      );
+
+      if (packageFromAllPackages) {
+        const tasks = (packageFromAllPackages.tasks || []).map(task => ({
+          taskId: task._id,
+          startDate: new Date().toISOString(),
+        }));
+
+        formattedData.packages.push({
+          packageId: packageFromAllPackages._id,
+          tasks: tasks,
+        });
+      }
+    });
+
+    updateEmployeePackageByAdmin(formattedData)
+      .then(() => {
+        showSuccessToast('Successfully updated !');
+      })
+      .catch(error =>
+        toast.error(error.response?.data?.message || 'Something went wrong')
+      );
+  };
 
   return (
     <BgDiv>
@@ -47,10 +95,7 @@ const PackagesFormComponent = ({
             organizationConfig.organization_theme.theme.backgroundColor,
         }}
         className="rounded-lg shadow-lg w-full p-8"
-        onSubmit={(e) => {
-          e.preventDefault();
-          showSuccessToast("Packages added");
-        }}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <Controller
           name="packagesInfo"
@@ -58,15 +103,18 @@ const PackagesFormComponent = ({
           render={({ field }) => (
             <MultiSelect
               className="mb-2"
-              data={employmentPackagesOptions}
+              data={employmentPackagesOptions.map((res: any) => ({
+                value: res._id,
+                label: res.title,
+              }))}
               label="Packages"
               placeholder="Select Packages"
               value={(field.value ?? []).filter(
-                (pkg: any): pkg is string => typeof pkg === "string"
+                (pkg: any): pkg is string => typeof pkg === 'string'
               )}
               onChange={field.onChange}
               onBlur={field.onBlur}
-              error={errors.packagesInfo?.message}
+              error={errors.packagesInfo?.message as any}
             />
           )}
         />
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/.git/**', 'dist', 'build'],
+    },
+    hmr: {
+      overlay: false,
+    },
+  },
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
-})
+    sourcemap: false,
+  },
+});
