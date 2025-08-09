bd0172c608ef25836154e068c243d76c0b6268d7
saibabuchimma-dev <saibabuchimma@gmail.com>
2025-08-10 00:24:06 +0530

    Implement useMemo and useCallback hooks in Pool Candidates menu

--- src/interfaces/candidate.ts
+++ src/interfaces/candidate.ts
@@ -1,5 +1,6 @@
 export interface PoolCandidatesComments {
   userId: {
+    _id: string;
     firstName: string;
     lastName: string;
     id: string;
@@ -8,5 +9,28 @@ export interface PoolCandidatesComments {
   callEndsAt: Date;
   comment: string;
   updateAt: string;
+  _id: string;
   id: string;
+}
+
+export interface CandidateInterface {
+  _id: string;
+  candidateName: string;
+  contact: {
+    email: string;
+    phone: string;
+  };
+  totalYearsOfExperience: number;
+  relaventYearsOfExperience: number;
+  evaluatedSkills: string;
+  comments: PoolCandidatesComments[];
+  createdBy: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    id: string;
+  };
+  createdAt: string;
+  lastUpdatedAt: string;
+  __v: number;
 }
