34051d6573ae1ffedfa6ed4952bb0085d6eed5b6
Ammu20029 <prasannatalada748@gmail.com>
2026-02-09 22:34:40 +0530

    updating salary submenus

--- src/atoms/employee-atom.ts
+++ src/atoms/employee-atom.ts
@@ -4,25 +4,25 @@ import { PackagesList } from '../interfaces/package';
 
 export const employeeDetailsAtom = atom<EmployeeUpdateForm | null>({
   key: 'employeeDetailsAtom',
-  default: null,
+  default: null
 });
 
 export const bloodGroupOptionsAtom = atom({
   key: 'bloodGroupOptionsAtom',
-  default: null,
+  default: []
 });
 
 export const employmentTypesAtom = atom({
   key: 'employmentTypesAtom',
-  default: null,
+  default: []
 });
 
 export const employeeRolesAtom = atom({
   key: 'employeeRolesAtom',
-  default: null,
+  default: []
 });
 
 export const employeePackagesAtom = atom<PackagesList | null>({
   key: 'employeePackagesAtom',
-  default: null,
+  default: null
 });
