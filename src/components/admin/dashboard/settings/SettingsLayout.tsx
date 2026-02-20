ce179cc3959577f9b2ee0ecf9120422c8bb3f068
Ammu20029 <prasannatalada748@gmail.com>
2026-02-20 16:07:34 +0530

    fixing UI related issues

--- src/components/admin/dashboard/settings/SettingsLayout.tsx
+++ src/components/admin/dashboard/settings/SettingsLayout.tsx
@@ -1,16 +1,18 @@
-import { Tabs, Container } from '@mantine/core';
+import { Tabs, Container, Box } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
 
 const SettingsLayout = () => {
   return (
-    <Container size="lg" mt="lg" mb="lg">
-      <Tabs value="feedback" variant="outline" mb="xs">
-        <Tabs.List>
-          <Tabs.Tab value="feedback">Feedback</Tabs.Tab>
-        </Tabs.List>
-      </Tabs>
-      <Outlet />
-    </Container>
+    <Box pt={{ base: 'xl', sm: 'md' }}>
+      <Container size="md" mt={{ base: 'xl', sm: 'lg' }} mb="lg">
+        <Tabs value="feedback" variant="outline" mb="xs">
+          <Tabs.List>
+            <Tabs.Tab value="feedback">Feedback Attributes</Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
+        <Outlet />
+      </Container>
+    </Box>
   );
 };
 
